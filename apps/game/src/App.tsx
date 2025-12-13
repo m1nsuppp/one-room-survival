edb1b974c78ad52966b3d0cabfc15b9c8a1d1d87
Minsup Lee <m1nsuppp@gmail.com>
2025-12-13 23:12:42 +0900

    chore(game): config eslint

--- apps/game/src/App.tsx
+++ apps/game/src/App.tsx
@@ -1,28 +1,33 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import type { JSX } from 'react';
 
-function Room() {
+function Room(): JSX.Element {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+    <mesh
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, 0, 0]}
+    >
       <planeGeometry args={[4, 4]} />
       <meshStandardMaterial color="#f0f0f0" />
     </mesh>
   );
 }
 
-function App() {
+export default function App(): JSX.Element {
   return (
     <Canvas
       camera={{ position: [5, 5, 5], fov: 50 }}
       style={{ width: '100vw', height: '100vh' }}
     >
       <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight
+        position={[10, 10, 5]}
+        intensity={1}
+      />
       <Room />
       <OrbitControls />
       <gridHelper args={[4, 4]} />
     </Canvas>
   );
 }
-
-export default App;
